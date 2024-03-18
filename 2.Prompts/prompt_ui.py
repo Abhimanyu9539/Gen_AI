7d0bda430578be7fb67bacb5102baebc9b621a60
Abhimanyu9539 <iabhimanyu31@gmail.com>
2024-03-18 11:00:00 +0530

    Prompts with UI

---
+++ 2.Prompts/prompt_ui.py
@@ -0,0 +1,13 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import streamlit as st
+
+load_dotenv()
+st.header("Reasearch Tool")
+
+model = ChatOpenAI(model = "gpt-4")
+user_input = st.text_input('Enter Your Input')
+
+if st.button('Summarize'):
+    result = model.invoke(user_input)
+    st.write(result.content)
