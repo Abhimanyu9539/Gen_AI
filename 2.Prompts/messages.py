22e22701446dbb3daef3a82123f54f4cb9120b47
Abhimanyu9539 <iabhimanyu31@gmail.com>
2025-03-20 22:58:23 +0530

    Create langchain messages

--- 2.Prompts/messages.py
+++ 2.Prompts/messages.py
@@ -6,3 +6,12 @@ load_dotenv()
 
 model = ChatOpenAI()
 
+messages = [
+    SystemMessage(content="You are a helpful assistant."),
+    HumanMessage(content="Tell me about Langchain")
+]
+
+result = model.invoke(messages)
+messages.append(AIMessage(content=result.content))
+
+print(messages)
